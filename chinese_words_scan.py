7e856aa257f481b80951d653f275e703ac428148
password123456 <sendmefuture@gmail.com>
2015-09-09 17:04:25 +0900

    Update chinese_words_scan.py

--- chinese_words_scan.py
+++ chinese_words_scan.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -13,10 +12,9 @@ import codecs
 import argparse
 import re
 
-def do_scan_chinese(filename):
+def DO_SCAN_CHINESE(filename):
     f = codecs.open(filename, 'r', 'utf-8')
-
-    iCHINESE_WORDS_COUNTER = 0
+    iCHINESE_WORDS_COUNT = 0
     bCHINESE_DETECT = 'false'
 
     for n,line in enumerate(f.read().split('\n')):
@@ -24,7 +22,7 @@ def do_scan_chinese(filename):
         n += 1
         if re.findall(ur'[\u4e00-\u9fff]+', line):
             bCHINESE_DETECT = 'true'
-            iCHINESE_WORDS_COUNTER += 1
+            iCHINESE_WORDS_COUNT += 1
             #print "line:",n,line
         else:
             pass
@@ -32,31 +30,34 @@ def do_scan_chinese(filename):
     f.close()
 
     if bCHINESE_DETECT.lower() in ['true']:
-        print ' + [Chinese /',iCHINESE_WORDS_COUNTER,'] ', filename
+        print ' + [Chinese /',iCHINESE_WORDS_COUNT,'] ', filename
     else:
-        print ' + [Chinese /',iCHINESE_WORDS_COUNTER,'] ', filename
+        print ' + [Chinese /',iCHINESE_WORDS_COUNT,'] ', filename
 
-def scan_file(path):
-    #print "Current Path: " + path
-    for root, dirs, files in os.walk(path):
+def SCAN_TEXTFILE(path):
+    if os.path.exists(path):
+	    for root, dirs, files in os.walk(path):
 
-        for filename in files:
-            file = os.path.realpath(os.path.join(root,filename))
-            try:
-                with open(file, 'rb') as f:
-                    if b'\x00' in f.read():
-                        continue
-                        #print('The file is binary!', file)
-                    else:
-                        #print('The file is not binary!', file)
-                        do_scan_chinese(file)
-            except:
-                continue
+	        for filename in files:
+	            file = os.path.realpath(os.path.join(root,filename))
+	            try:
+	                with open(file, 'rb') as f:
+	                    if b'\x00' in f.read():
+	                        continue
+	                        #print('The file is binary!', file)
+	                    else:
+	                        #print('The file is not binary!', file)
+	                        DO_SCAN_CHINESE(file)
+	            except:
+	                pass
+    else:
+        print "\n[-] [ %s ] not exits.! Check directory.!! " % (path)
+        sys.exit()
 
 def main():
     opt=argparse.ArgumentParser(description="::::: Chinese Words Scanner :::::")
     opt.add_argument("scan_path", help="ex) /chinese_textable_path")
-    opt.add_argument("-p", "--path", action="store_true", dest="path", help="ex) python chinese_words_scan_v0.1.py -p /chinese_textable_path")
+    opt.add_argument("-p", "--path", action="store_true", dest="path", help="ex) python chinese_words_scan_v0.2.py -p /chinese_textable_path")
 
     if len(sys.argv)<=2:
         opt.print_help()
@@ -66,7 +67,7 @@ def main():
 
     if options.path:
         path = (options.scan_path)
-        scan_file(path)
+        SCAN_TEXTFILE(path)
 
     else:
         opt.print_help()
